cf4973cffc003d8b1fc241d4eb86c760890e76bb
Franco Morroni <fmorroni99@gmail.com>
2023-12-29 15:56:47 -0300

    Feat <lvim>: add telescope-undo plugin

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -53,6 +53,38 @@ lvim.plugins = {
       vim.keymap.set('n', '<a-l>', require('tree-climber').swap_next, keyopts)
     end
   },
+  {
+    "debugloop/telescope-undo.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
+    keys = {
+      { -- lazy style key map
+        "<leader>u",
+        "<cmd>Telescope undo<cr>",
+        desc = "undo history",
+      },
+    },
+    opts = {
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = { preview_height = 0.6, height = 0.95, width = 0.8 }
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      require("telescope").setup(opts)
+      require("telescope").load_extension("undo")
+    end,
+  },
   -- "sakhnik/nvim-gdb",
   -- {
   --   'glacambre/firenvim',
