24844a2ce03179faa708248fcddd654773e72a9c
Franco Morroni <fmorroni99@gmail.com>
2024-01-30 01:27:39 -0300

    Feat <lvim>: put lsp completions first in the list

--- .config/lvim/lua/user/cmp.lua
+++ .config/lvim/lua/user/cmp.lua
@@ -1,8 +1,9 @@
 -- Put luasnips first in the list of recommendations.
 local sources = lvim.builtin.cmp.sources
 for i, source in pairs(sources) do
-  if source.name == "luasnip" then
+  if source.name == "luasnip" or source.name == "nvim_lsp" then
     table.remove(sources, i)
   end
 end
-table.insert(sources, 1, { name = "luasnip" })
+table.insert(sources, 1, { name = "nvim_lsp" })
+table.insert(sources, 2, { name = "luasnip" })
