95ba6ad91c45250a3afef6561320d61d50bc38f1
Franco Morroni <fmorroni99@gmail.com>
2024-02-17 06:35:14 -0300

    Feat: add yazi config

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -171,3 +171,12 @@ compdef _rm configadd
 function confignew() {
   config commit -m "Feat: add $1 config"
 }
+
+function ya() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd "$cwd"
+	fi
+	\rm -f -- "$tmp"
+}
