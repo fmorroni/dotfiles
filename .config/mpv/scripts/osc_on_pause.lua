ec113a545b56d89642e7bc08e08e223272635572
Franco Morroni <fmorroni99@gmail.com>
2025-09-12 12:35:37 -0300

    chore <mpv>: idk something with the progress bar

--- .config/mpv/scripts/osc_on_pause.lua
+++ .config/mpv/scripts/osc_on_pause.lua
@@ -1,4 +1,4 @@
-function on_pause_change(name, value)
+function on_pause_show_progress_bar(name, value)
   if value == true then
     mp.commandv("script-message", "osc-visibility", "always", "no-osd")
   else
@@ -7,4 +7,13 @@ function on_pause_change(name, value)
    end)
   end
 end
-mp.observe_property("pause", "bool", on_pause_change)
+mp.observe_property("pause", "bool", on_pause_show_progress_bar)
+
+-- function on_seek_show_progress_bar()
+--   print("seek")
+--   mp.commandv("script-message", "osc-visibility", "always", "no-osd")
+--   mp.add_timeout(0.5, function() 
+--     mp.commandv("script-message", "osc-visibility", "auto", "no-osd")
+--   end)
+-- end
+-- mp.observe_property("playback-time", "native", on_seek_show_progress_bar)
