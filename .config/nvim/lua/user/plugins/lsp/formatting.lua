4011949c8dbb022d008126fe0419d4efaff08609
Franco Morroni <fmorroni99@gmail.com>
2025-09-28 17:09:40 -0300

    feat <nvim>: formatting

---
+++ .config/nvim/lua/user/plugins/lsp/formatting.lua
@@ -0,0 +1,49 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = {
+    'mason-org/mason.nvim',
+    'mason-org/mason-lspconfig.nvim',
+  },
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>lf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+    },
+    -- Set default options
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        append_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
