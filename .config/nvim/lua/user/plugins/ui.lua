59c017e910f3d4d0ba6f3f5e427095dcc364ca1a
Franco Morroni <fmorroni@fu.do>
2026-01-17 03:05:17 -0300

    chore <nvim>: show file relative path instead of name in lualine

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -19,7 +19,10 @@ return {
         lualine_x = { "%S", "", "encoding", "fileformat", "filetype" },
         lualine_y = { "selectioncount", "progress" },
         lualine_c = {
-          "filename",
+          {
+            'filename',
+            path = 1,
+          },
           {
             "macro",
             fmt = function()
@@ -82,11 +85,11 @@ return {
         },
       },
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
       },
     },
     dependencies = {
