e30199d04e942cf08ce27ffa9fe176842477a62e
Franco Morroni <fmorroni@fu.do>
2026-05-11 23:26:55 -0300

    feat <nvim>: better file copy command for Snacks Explorer
    I initiates the file name field with the copied file's name instead of empty.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -39,6 +39,7 @@ return {
                 keys = {
                   ["y"] = "yank_relative_cwd",
                   ["Y"] = "yank_relative_home",
+                  ["c"] = "explorer_copy_default",
                 },
               },
             },
@@ -54,6 +55,39 @@ return {
                 vim.fn.setreg("+", path)
                 vim.fn.setreg('"', path)
                 vim.notify("Yanked: " .. path)
+              end,
+              explorer_copy_default = function(picker, item)
+                if not item then return end
+                local Tree = require("snacks.explorer.tree")
+                local actions = require("snacks.explorer.actions")
+                local uv = vim.uv or vim.loop
+                ---@type string[]
+                local paths = vim.tbl_map(Snacks.picker.util.path, picker:selected())
+                -- Copy selection
+                if #paths > 0 then
+                  local dir = picker:dir()
+                  Snacks.picker.util.copy(paths, dir)
+                  picker.list:set_selected() -- clear selection
+                  Tree:refresh(dir)
+                  Tree:open(dir)
+                  actions.update(picker, { target = dir })
+                  return
+                end
+                Snacks.input({
+                  prompt = "Copy to",
+                  default = vim.fn.fnamemodify(item.file, ":t"),
+                }, function(value)
+                  if not value or value:find("^%s$") then return end
+                  local dir = vim.fs.dirname(item.file)
+                  local to = svim.fs.normalize(dir .. "/" .. value)
+                  if uv.fs_stat(to) then
+                    Snacks.notify.warn("File already exists:\n- `" .. to .. "`")
+                    return
+                  end
+                  Snacks.picker.util.copy_path(item.file, to)
+                  Tree:refresh(vim.fs.dirname(to))
+                  actions.update(picker, { target = to })
+                end)
               end,
             },
           },
