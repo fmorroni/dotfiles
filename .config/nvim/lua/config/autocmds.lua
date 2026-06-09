d1fb6c64ad7d6fc0ed4e3bd1e1db94688f4f3c64
Franco Morroni <fmorroni99@gmail.com>
2026-06-08 21:36:05 -0300

    feat <nvim>: change cwd when using `nvim <path>`

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  callback = function()
+    local arg = vim.fn.argv(0)
+
+    if arg == "" then return end
+
+    local cwd
+
+    if vim.fn.isdirectory(arg) == 1 then
+      cwd = vim.fn.fnamemodify(arg, ":p")
+    else
+      cwd = vim.fn.fnamemodify(arg, ":p:h")
+    end
+
+    vim.cmd.cd(vim.fn.fnameescape(cwd))
+  end,
+})
