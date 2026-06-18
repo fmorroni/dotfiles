27c84895ca86a7461a01abf2848475dcc920fed7
Franco Morroni <fmorroni@fu.do>
2026-06-18 16:16:27 -0300

    chore <nvim>: prettierd formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,9 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        -- ["markdown"] = { { "prettierd", "prettier" }, "markdown-toc" },
-        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-        -- ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
       },
     },
   },
