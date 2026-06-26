dcc426ce785758f3c011e002d7a98027d8ddbb6d
Franco Morroni <fmorroni@fu.do>
2026-06-26 16:10:19 -0300

    chore <nvim>: conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,9 +3,10 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        -- ["markdown"] = { { "prettierd", "prettier" }, "markdown-toc" },
-        ["markdown"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
-        -- ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        markdown = { "prettierd", "markdownlint-cli2", "markdown-toc" },
+        typescriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        javascript = { "prettierd" },
       },
     },
   },
