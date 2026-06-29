e18a045c0cb57573d7c4f307f0ffc0a9f38e4bc7
Franco Morroni <fmorroni99@gmail.com>
2026-06-29 11:55:50 -0300

    chore <nvim>: conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,32 @@
+local livecalc_ts_dir = vim.fn.expand("~/projects/livecalc/tree-sitter-livecalc")
+
 return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        livecalc = {
+          command = vim.fn.expand("~/.local/share/cargo/bin/topiary"),
+          args = {
+            "format",
+            "--configuration",
+            livecalc_ts_dir .. "/topiary.ncl",
+            "--language",
+            "livecalc",
+          },
+          env = {
+            TOPIARY_LANGUAGE_DIR = livecalc_ts_dir .. "/queries/topiary",
+          },
+          stdin = true,
+        },
+      },
       formatters_by_ft = {
         markdown = { "prettierd", "markdownlint-cli2", "markdown-toc" },
         typescriptreact = { "prettierd" },
-        typescript= { "prettierd" },
-        javascript= { "prettierd" },
+        typescript = { "prettierd" },
+        javascript = { "prettierd" },
+        livecalc = { "livecalc" },
+        nickel = { "prettierd" },
       },
     },
   },
