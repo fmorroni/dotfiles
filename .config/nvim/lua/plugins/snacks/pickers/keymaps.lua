71c49defcb0a43a598de281ed34a5eb270d0a88f
Franco Morroni <fmorroni@fu.do>
2026-08-07 12:11:42 -0300

    feat <nvim>: snacks pickers keymaps

--- .config/nvim/lua/plugins/snacks/pickers/keymaps.lua
+++ .config/nvim/lua/plugins/snacks/pickers/keymaps.lua
@@ -5,6 +5,7 @@ return {
     ["<c-b>"] = false,
     ["<c-f>"] = false,
     ["<a-i>"] = false,
+    ["<c-n>"] = false,
     -------------
     ["<a-j>"] = { "preview_scroll_down", mode = { "i", "n" } },
     ["<a-k>"] = { "preview_scroll_up", mode = { "i", "n" } },
@@ -12,8 +13,8 @@ return {
     ["<a-h>"] = { "preview_scroll_left", mode = { "i", "n" } },
     ["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
     ["<c-i>"] = { "toggle_ignored", mode = { "i", "n" } },
-    ["<C-n>"] = { "history_forward", mode = { "i", "n" } },
-    ["<C-p>"] = { "history_back", mode = { "i", "n" } },
+    ["<C-p>"] = { "history_forward", mode = { "i", "n" } },
+    ["<C-o>"] = { "history_back", mode = { "i", "n" } },
     ["<a-p>"] = { "toggle_only_preview", mode = { "i", "n" } },
     ["<a-P>"] = { "toggle_preview", mode = { "i", "n" } },
     ["<c-l>"] = { "focus_preview", mode = { "i", "n" } },
@@ -27,5 +28,7 @@ return {
     ["<c-h>"] = { "focus_input_normal_mode" },
     ["<c-l>"] = { "focus_input_normal_mode" },
     ["<a-m>"] = { "toggle_maximize" },
+    ["<c-j>"] = { "list_down" },
+    ["<c-k>"] = { "list_up" },
   },
 }
